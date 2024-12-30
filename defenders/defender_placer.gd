1bcc0a6b45d389b4ee2388dd2b61f8542f8befb8
TK <developer.aungkaungkhant@gmail.com>
2024-12-31 00:12:14 +0700

    feat: check occupied tile before place

--- defenders/defender_placer.gd
+++ defenders/defender_placer.gd
@@ -1,12 +1,18 @@
 extends Panel
 
 @onready var GAME = get_node("/root/Main")
-@onready var TILEMAP = get_node("/root/Main/DefenderTileMapLayer")
+@onready var TILEMAP = get_node("/root/Main/BgTileMapLayer")
+
+var occupied_cells = [];
 
 func finish_picking_defender():
 	GAME.action = 'default'
 	GAME.picked_defender = null
 	GAME.spend_essence()
+
+func cancel_picking_defender():
+	GAME.action = 'default'
+	GAME.picked_defender = null
 
 func _physics_process(delta: float) -> void:
 	if GAME.action == "picking_defender":
@@ -14,7 +20,7 @@ func _physics_process(delta: float) -> void:
 	else:
 		visible = false
 
-func get_clicked_cell():
+func get_clicked_cell(): 
 	var click_position = TILEMAP.get_local_mouse_position()
 	var clicked_cell = TILEMAP.local_to_map(click_position)
 	return clicked_cell
@@ -24,14 +30,20 @@ func add_defender_to_cell(defender: Node, cell: Vector2i):
 		var cell_position = TILEMAP.map_to_local(cell)
 		defender.position = cell_position
 		TILEMAP.add_child(defender)
+		occupied_cells.push_back(cell)
 	else:
 		print("Invalid cell")
 
 func place_defender():
 	var defender = GAME.picked_defender.instantiate()
 	var cell = get_clicked_cell()
-	add_defender_to_cell(defender, cell)
-	finish_picking_defender()
+	var is_occupied = occupied_cells.has(cell)
+	
+	if not is_occupied:
+		add_defender_to_cell(defender, cell)
+		finish_picking_defender()
+	else:
+		cancel_picking_defender()
 
 func _on_gui_input(event: InputEvent) -> void:
 	if GAME.action == 'picking_defender':
