393b07bc55c08a9c901a9087d23e8e96071c4f49
TK <developer.aungkaungkhant@gmail.com>
2025-01-02 23:49:08 +0700

    feat: restart game

---
+++ ui/restart_button.gd
@@ -0,0 +1,8 @@
+extends PanelContainer
+
+func _on_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.button_index == 1 and event.is_pressed():
+		get_tree().paused = false
+		get_tree().reload_current_scene()
+		%GameOverScreen.visible = false
+		%VictoryScreen.visible = false
